e9c2d75a3afb42e4dc51e6785e14eae018acd9be
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-13 20:43:07 +0100

    feat : add a new condition if the number is negative

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = int(repr(number)[-1])
 
-if number > 0 :
+if number > 0:
+    last_digit = int(repr(number)[-1])
     print(f"Last digit of {number} is {last_digit}", end = " ")
 else:
-    print(f"Last digit of {number} is -{last_digit}", end = " ")
+    last_digit = int(repr(number)[0] + repr(number)[-1])
+    print(f"Last digit of {number} is {last_digit}", end = " ")
 
 if last_digit > 5:
     print("and is greater than 5")
