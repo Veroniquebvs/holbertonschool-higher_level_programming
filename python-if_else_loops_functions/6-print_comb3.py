bb6acd6ffcec851660d98ce543ea1c04183c8a01
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-14 17:27:15 +0100

    Fix : correction print function with .format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 for i in range(0, 9):
     for j in range(1, 10):
         if i == 8 and j == 9:
-            print(f"{i}{j}")
+            print("{}{}".format(i, j))
         elif i < j:
-            print(f"{i}{j}, ", end="")
+            print("{}{}, ".format(i, j), end="")
         else:
             continue
