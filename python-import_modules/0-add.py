c3cbeaedc01781e98e6457e816acc798c0c0343c
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-15 12:52:23 +0100

    Fix : delete the word add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
+
 if __name__ == "__main__":
     a = 1
     b = 2
-    resultat = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, resultat))
+    print("{} + {} = {}".format(a, b, add(a, b)))
