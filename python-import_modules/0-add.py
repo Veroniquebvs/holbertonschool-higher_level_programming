86a571fbb5e4c9fbcf7349fabca52f96136b08f2
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-15 11:52:47 +0100

    Fix : add if __name__ __main__

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import add_0
-a = 1
-b = 2
-resultat = add_0.add(a, b)
-print("{} + {} = {}".format(a, b, resultat))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    resultat = add_0.add(a, b)
+    print("{} + {} = {}".format(a, b, resultat))
