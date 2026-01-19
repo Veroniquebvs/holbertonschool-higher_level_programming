5418f56fc689918ecaf20d5719ca0bd80be96adc
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-19 12:06:18 +0100

    Fix : change list of integers in list of  string

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    str_my_list = str(my_list)
-
-    for x in str_my_list:
-        if x == "[" or x == "," or x == " " or x == "]":
-            continue
-        print("{}".format(x))
+    for x in my_list:
+        print("{:}".format(x))
