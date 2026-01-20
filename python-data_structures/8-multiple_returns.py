ca134eb6fc834b3d4757539d2a3323d4f3994984
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-20 10:08:09 +0100

    Fix : change order condition

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if sentence:
-        new_tuple = (len(sentence), sentence[0])
-        return new_tuple
-    return None
+    if len(sentence) == 0:
+        return (0, None)
+    return (len(sentence), sentence[0])
