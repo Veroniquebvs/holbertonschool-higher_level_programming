95093a8eb6d572417a7d2af6a6d43c1ceca8e774
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-21 18:52:10 +0100

    Fix : change name exception

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_integer(value):
-    if isinstance(value, int):
-        try:
-            print("{}".format(value))
-            return True
-        except NameError:
-            print("NameError")
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
