b2da11b06a88b5fda9c7d3090a4d8f0b788270aa
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-22 15:05:32 +0100

    Create a function that prints the first x elements of a list and only integers

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,9 +3,3 @@
 def multiply_by_2(a_dictionary):
     new_dict = {key: value * 2 for key, value in a_dictionary.items()}
     return new_dict
-
-
-# prix_euros = {"pomme": 1.5, "banane": 0.8, "orange": 2.0}
-# Convertir en dollars (taux 1.1)
-# prix_dollars = {fruit: prix * 1.1 for fruit, prix in prix_euros.items()}
-# print(prix_dollars)  # {'pomme': 1.65, 'banane': 0.88, 'orange': 2.2}
