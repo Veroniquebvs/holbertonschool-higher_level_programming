35a10879047f0b140171388563c9d8a363fec389
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-23 20:01:55 +0100

    Fix : modification pycodestyle

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Module that prints a text with 2 new lines after each of these characters: ., ? and :
+Module that prints a text with 2 new lines
+after each of these characters: ., ? and :
 """
 
 
