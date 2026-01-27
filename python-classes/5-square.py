64090aae1e86705aa1db7a514c5d7740df4bb58a
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-27 10:49:42 +0100

    Fix : change the parametre's name "size" by "value"

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -23,15 +23,15 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """
         This method sets the size of the square
         size must be an integer"""
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
 
     def area(self):
         """
