dfe97e9c41dca69ca389ac09b0097ee9ae03b96a
veroniquebvs <beauvais.veronique@gmail.com>
2026-01-28 10:15:47 +0100

    Fix : correction of validation conditions for position parameter

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -47,7 +47,9 @@ class Square:
         """
         This method sets the position of the square
         """
-        if isinstance(value[0], int) and isinstance(value[1], int):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) and not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
         if value[0] < 0 and value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
