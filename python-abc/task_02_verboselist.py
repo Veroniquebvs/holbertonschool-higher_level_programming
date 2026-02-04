5d37977f9fdcc5a09d1e3a1e4e843897f2afe3b2
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-04 18:16:54 +0100

    Ajoute la classe VerboseList avec des méthodes personnalisées pour append, extend, remove et pop

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+class VerboseList(list):
+    def append(self, item):
+        super().append(item)
+        print(f"Added {item} to the list.")
+
+    def extend(self, item):
+        super().extend(item)
+        print(f"Extended the list with [{len(item)}] items.")
+
+    def remove(self, item):
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, i=-1):
+        item = self[i]
+        print(f"Popped [{item}] from the list.")
+        super().pop(i)
