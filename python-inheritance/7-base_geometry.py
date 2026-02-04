13d6971d46fcfe97d51a63bbb1bcc118b0d07f32
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-04 12:34:30 +0100

    Corrige la documentation de la classe BaseGeometry et améliore la validation des entiers

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Define a BaseGeometry class
+Defines a BaseGeometry class
 """
 
 
@@ -8,11 +8,12 @@ class BaseGeometry:
     """
     Represent base geometry
     """
+
     def area(self):
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
