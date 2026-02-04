d58518b56f9e9317024dad231019ccef940237d0
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-04 10:17:28 +0100

    Remove unnecessary documentation in the MyList class

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
-"""
-This module defines MyList
-"""
-
 
 class MyList(list):
     def print_sorted(self):
-        """
-        Prints the list sorted in ascending order
-        """
         print(sorted(self))
-
