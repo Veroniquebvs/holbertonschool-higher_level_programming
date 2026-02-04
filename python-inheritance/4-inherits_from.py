75d7e075cae0fa9c3e03009d1f1b6510147901fb
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-04 12:40:22 +0100

    Corrige la logique de validation dans la fonction inherits_from

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,9 +7,9 @@ from a specified class (but is not exactly that class)
 
 def inherits_from(obj, a_class):
     """
-     Returns True if obj is an instance of a class that
+    Returns True if obj is an instance of a class that
     inherits from a_class (directly or indirectly),
     but False if obj is exactly of type a_class
     """
 
-    return isinstance(type(obj), a_class) and type(obj) is not a_class
+    return isinstance(obj, a_class) and type(obj) is not a_class
