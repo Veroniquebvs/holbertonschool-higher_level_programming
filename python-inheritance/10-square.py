fdc2d5cdcfa8c170b597bbee0a43045d456fbe84
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-04 11:18:43 +0100

    Reorganise imports in the Square module for better readability

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-Rectangle = __import__('9-rectangle').Rectangle
-
 """
 This module defines a square
 """
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
