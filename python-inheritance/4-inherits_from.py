b13b4ddf6444d4da1eacbe3a6d1d71999a5d7bc7
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-04 13:22:15 +0100

    Clarifie la documentation de la fonction inherits_from pour améliorer la compréhension

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 """
-This module checks whether an object inherits
-from a specified class (but is not exactly that class)
+Check if an object inherits from a class
+but is not exactly that class
 """
 
 
 def inherits_from(obj, a_class):
-    """
-    Returns True if obj is an instance of a class that
-    inherits from a_class (directly or indirectly),
-    but False if obj is exactly of type a_class
-    """
+    """Return True if obj is an instance of a subclass of a_class"""
     if type(obj) is a_class:
         return False
     return isinstance(obj, a_class)
