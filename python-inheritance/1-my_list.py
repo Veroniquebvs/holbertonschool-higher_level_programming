f0d696e5effe3beb343a616cac276a5f81910229
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-04 12:58:14 +0100

    Ajoute des docstrings pour clarifier le module et la classe MyList

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+This module prints a list
+"""
+
 
 class MyList(list):
+    """
+    The class MyList inherits from the class list
+    """
     def print_sorted(self):
-        print(sorted(self))
+        """
+        This function prints a sorted list
+        """
+        new_list = sorted(self)
+        print(new_list)
