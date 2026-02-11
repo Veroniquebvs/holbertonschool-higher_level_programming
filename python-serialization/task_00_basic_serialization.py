263b4b18afcc03a9e277b1398d61286a17b195a6
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-11 14:46:59 +0100

    fix : change name of the parameter

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,11 +5,11 @@ import json
 
 def serialize_and_save_to_file(data, filename):
     # Your code here to serialize and save data to the specified file
-    with open("data.json", "w") as filename:
-        json.dump(data, filename)
+    with open(filename, "w") as f:
+        json.dump(data, f)
 
 
 def load_and_deserialize(filename):
     # Your code here to load and deserialize data from the specified file
-    with open("data.json", "r") as filename:
-        return json.load(filename)
+    with open(filename, "r") as f:
+        return json.load(f)
