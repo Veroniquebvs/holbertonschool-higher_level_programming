5179064f67d81e8a02f89c3ac7181cd5dcce157e
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-11 18:09:19 +0100

    Create a class with serialize and deserialize method

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        if not isinstance(name, str):
+            raise TypeError("name must be a string")
+        self.name = name
+
+        if not isinstance(age, int):
+            raise TypeError("age must be an integer")
+        self.age = age
+
+        if not isinstance(is_student, bool):
+            raise TypeError("is_student must be an bool")
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        try:
+            with open(filename, "wb") as f:
+                pickle.dump(self, f)
+        except (FileNotFoundError, FileExistsError):
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        try:
+            with open(filename, "rb") as f:
+                return pickle.load(f)
+        except FileNotFoundError:
+            return None
