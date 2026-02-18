4a4e14017aaa75f4bb63482bcd903793fc38745a
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-18 11:47:12 +0100

    Fix : Add condition if success

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -5,23 +5,23 @@ import csv
 
 def fetch_and_print_posts():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    r.raise_for_status()
-    print(f"Status Code: {r.status_code}")
-    new_data = r.json()
-    for item in new_data:
-        print(item.get("title"))
+    if r.status_code == 200:
+        print(f"Status Code: {r.status_code}")
+        new_data = r.json()
+        for item in new_data:
+            print(item.get("title"))
 
 
 def fetch_and_save_posts():
     r = requests.get('https://jsonplaceholder.typicode.com/posts')
-    r.raise_for_status()
-    new_data = r.json()
-    post_dict = {}
-    new_list = []
-    for item in new_data:
-        post_dict = {"id": item.get("id"), "title": item.get("title"),
-                     "body": item.get("body")}
-        new_list.append(post_dict)
+    if r.status_code == 200:
+        new_data = r.json()
+        post_dict = {}
+        new_list = []
+        for item in new_data:
+            post_dict = {"id": item.get("id"), "title": item.get("title"),
+                         "body": item.get("body")}
+            new_list.append(post_dict)
 
     csv_file = "posts.csv"
     with open(csv_file, "w", newline='', encoding='utf-8') as f:
