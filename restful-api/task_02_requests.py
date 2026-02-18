41338d8ea8d1f7c8f0dd1be24ee22c0ca831473b
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-18 11:19:25 +0100

    Create a basic Python script to fetch posts from JSONPlaceholder

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,31 @@
+import requests
+import json
+import csv
+
+
+def fetch_and_print_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    r.raise_for_status()
+    print(f"Status Code: {r.status_code}")
+    new_data = r.json()
+    for item in new_data:
+        print(item.get("title"))
+
+
+def fetch_and_save_posts():
+    r = requests.get('https://jsonplaceholder.typicode.com/posts')
+    r.raise_for_status()
+    new_data = r.json()
+    post_dict = {}
+    new_list = []
+    for item in new_data:
+        post_dict = {"id": item.get("id"), "title": item.get("title"),
+                     "body": item.get("body")}
+        new_list.append(post_dict)
+
+    csv_file = "posts.csv"
+    with open(csv_file, "w", newline='', encoding='utf-8') as f:
+        fieldnames = new_list[0].keys()
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(new_list)
