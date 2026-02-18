cfbd7a16fc8f6cd1bd17d90b65f91863908b7850
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-18 16:19:10 +0100

    Create a simple API

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,45 @@
+import http.server
+import socketserver
+import json
+
+
+class my_new_class(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/data":
+            data = {"name": "John", "age": 30, "city": "New York"}
+            new_dataset = json.dumps(data)
+            my_content = new_dataset
+            content_type = "application/json"
+            self.response(my_content, content_type)
+        elif self.path == "/info":
+            data = {"version": "1.0", "description": "A simple "
+                    "API built with http.server"}
+            new_dataset = json.dumps(data)
+            my_content = new_dataset
+            content_type = "application/json"
+            self.response(my_content, content_type)
+        elif self.path == "/status":
+            my_content = "OK"
+            content_type = "text/plain"
+            self.response(my_content, content_type)
+        elif self.path == "/":
+            my_content = "Hello, this is a simple API!"
+            content_type = "text/plain"
+            self.response(my_content, content_type)
+
+        else:
+            self.send_error(404, "Endpoint not found")
+
+    def response(self, my_content, content_type):
+        my_content_bytes = my_content.encode('utf-8')
+        self.send_response(200)
+        self.send_header("Content-type", f"{content_type}; charset=utf-8")
+        self.send_header("Content-Length", str(len(my_content_bytes)))
+        self.end_headers()
+        self.wfile.write(my_content_bytes)
+
+
+PORT = 8000
+with socketserver.TCPServer(("", PORT), my_new_class) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
