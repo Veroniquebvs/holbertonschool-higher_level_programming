d4b9380dfcc7f8065316ca990826139cd58c89bc
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-20 09:28:39 +0100

    Create simple API with flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+users = {"jane": {"username": "jane", "name": "Jane",
+                  "age": 28, "city": "Los Angeles"},
+         "john": {"username": "john", "name": "John",
+                  "age": 30, "city": "New York"}}
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route("/data")
+def get_users():
+    return jsonify(list(users))
+
+
+@app.route("/status")
+def get_status():
+    return "OK"
+
+
+@app.route("/users/<username>")
+def get_username(username):
+    if username in users:
+        return jsonify(users[username])
+    else:
+        return {"error": "User not found"}, 404
+
+
+@app.route("/add_user", methods=["POST"])
+def create_user():
+    new_user = request.get_json()
+    if new_user is None:
+        return {"error": "Invalid JSON"}, 400
+    if "username" not in new_user:
+        return {"error": "Username is required"}, 400
+    if new_user["username"] in users:
+        return {"error": "Username already exists"}, 409
+    else:
+        username = new_user["username"]
+        users[username] = new_user
+        return jsonify(new_user), 201
+
+
+if __name__ == "__main__":
+    app.run()
