97ffa097a08a031e40d96c04087d903d32e8ec34
veroniquebvs <beauvais.veronique@gmail.com>
2026-02-20 10:08:04 +0100

    fix : change value of list returned

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -14,7 +14,7 @@ def home():
 
 @app.route("/data", methods=["GET"])
 def get_users():
-    return jsonify(list(users.keys))
+    return jsonify(list(users.keys()))
 
 
 @app.route("/status", methods=["GET"])
