b1749d4a4332c0f3d07cebfdace4039e547f2e15
veroniquebvs <beauvais.veronique@gmail.com>
2026-03-03 20:47:29 +0100

    Fix : Add README file and a line at the and of each files

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,9 +1,11 @@
+-- Create a table
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT
 );
 
+-- insert values
 INSERT INTO second_table (id, name, score)
 VALUES
 ('1', 'John', '10'),
