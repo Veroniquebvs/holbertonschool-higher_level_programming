76ae9c5c265b0dcd4a01ce034964c341e3c277f3
veroniquebvs <beauvais.veronique@gmail.com>
2026-03-05 20:39:04 +0100

    Create a script that takes in an argument and displays all values in the states table

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""a script that takes in an argument and displays all values in the states table"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+
+    sql = ("SELECT * FROM states WHERE name = %s ORDER BY states.id ASC")
+
+    cursor.execute(sql, (state_name_searched,))
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    db.close()
