22bccda2a34d0f5d6ff6d0b9acaba085af5aace5
veroniquebvs <beauvais.veronique@gmail.com>
2026-03-19 22:03:51 +0100

    Write a script that prints the addition of 2 integers

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const a = args[0];
+const b = args[1];
+
+if (isNaN(Number(a)) || isNaN(Number(b))) {
+  console.log('NaN');
+}
+
+else {
+ function add(a, b) {
+  console.log(a + b);
+ }
+ add(Number(a), Number(b));
+}
